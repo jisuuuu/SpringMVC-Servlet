206fd6909207d826180c079a160a11f1528e5760
Jisu Choi <gjkl55@naver.com>
2022-04-28 21:32:41 +0900

    [추가] 스프링 MVC - 등록, 저장, 조회 기능 MVC로 만들기

---
+++ src/main/java/hello/servlet/web/springmvc/v1/SpringMemberFormControllerV1.java
@@ -0,0 +1,18 @@
+package hello.servlet.web.springmvc.v1;
+
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+// 두 개가 Class 레벨에 있으면 handler로 인식
+//@Controller
+@Component
+@RequestMapping
+public class SpringMemberFormControllerV1 {
+
+    @RequestMapping("/springmvc/v1/members/new-form")
+    public ModelAndView process() {
+        return new ModelAndView("new-form");
+    }
+}
